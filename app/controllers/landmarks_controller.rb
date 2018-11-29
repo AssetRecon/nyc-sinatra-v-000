ea0fdb66a5f51addbe6fbb2bbfd3d3735b7428d1
Edsel Caprice <caprice@mail.usf.edu>
2018-11-29 11:58:09 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+get '/landmarks' do
+  erb :'/landmarks/index'
+end
+
+get '/landmarks/new' do
+  erb :'landmarks/new'
+end
+
+get '/landmarks/:id' do
+  erb '/landmarks/show'
+end
+
+get '/landamrks/:id/edit' do
+  erb :"/landmarks/edit"
+end
+
+  post '/landmarks' do
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  patch '/landmarks/:id'
+    redirect to "/landmarks/#{@landmark.id}"
 end
